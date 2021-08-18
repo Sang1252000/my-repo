ba621756f40f5617f821f3721fde9673e95b4db7
Sangamithra <msangamithra12@gmail.com>
2021-08-18 16:27:33 +0530

    initial

---
+++ HandsOnConstructors/Circle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnConstructors
+{
+    class Circle
+    {
+        public double radius;
+        static double PI;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        static Circle()
+        {
+            Console.WriteLine("Iam invoked only once");
+            PI = 3.14;
+        }
+        public double Area()
+        {
+            double result;
+            result = PI * radius * radius;
+            return result;
+        }
+    }
+    class Circle_Test
+    {
+        static void Main()
+        {
+            Circle[] c= new Circle[4];
+            c[0] = new Circle(10);
+            c[1] = new Circle(5);
+            c[2] = new Circle(2);
+            c[3] = new Circle(12);
+            foreach (var i in c)
+                Console.WriteLine("Area of  circle  with radius {0} is :{1}", i.radius, i.Area());
+
+
+
+        }
+    }
+}
---
+++ HandsOnConstructors/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HandsOnConstructors
+{
+    class Sample
+    {
+        int a;
+        int b;
+        public Sample()//default constructor
+        {
+            a = 10;
+            b = 20;
+        }
+        public Sample(int a, int b)//parameterized constructor
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Sample(Sample sample)//copy constructor
+        {
+            this.a = sample.a;
+            this.b = sample.b;
+        }
+        public void show()
+        {
+            Console.WriteLine("a :{0} b: {1}", a, b);
+        }
+
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sample s = new Sample();
+            s.show();
+            Sample s1 = new Sample(12,23);
+            s1.show();
+            Sample s2 = new Sample(s1);
+            s2.show();
+            Sample s3 = new Sample(s);
+            s3.show();
+
+
+        }
+    }
+}
